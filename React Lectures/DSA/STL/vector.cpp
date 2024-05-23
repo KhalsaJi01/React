27933f4739064194eb5b8affe35e9f3e357200f2
KhalsaJi01 <jagujagz12@gmail.com>
2024-05-23 12:28:22 +0530

    "STL vector Notes"

---
+++ React Lectures/DSA/STL/vector.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> v;//here we have created a empty vector . Vector is a dynamic array wich can change the size of the array 
+    //according to the requirement so you dont have take any tension about declaring the sizeb like you do in array. 
+    v.push_back(10);
+    v.push_back(20);
+    cout<<v.size()<<endl;//output is 2 this .size() tell us the size of the vector. how many entries are there
+    for(int i=0;i<v.size();i++)
+    {
+        cout<<v[i]<<endl;//output is 10 20
+    }
+    for (auto itr : v) // i have created the iterartor which will iterate and print the value in this i have used the short hand notation .auto is used so the it can auto detect the datatype and iterate over it
+    {
+        cout<<itr<<endl; // simply gives the same output 10 20
+    }
+    v.pop_back(); //.pop_back is used to remove the last element inside the vector
+    for (auto itr : v)
+    
+    {
+        cout<<itr<<endl; // it gives output 10
+    }
+    v.push_back(20);
+    int itr = v.front();//it returns the first element of the vector
+    int itr1 = v.back();// it returns the last element of the vector
+    cout<<itr<<endl;// output is 10
+    cout <<itr1<<endl;//output is 20
+    v.clear();// empty the vector
+    cout<<v.size()<<endl;
+    bool isEmpty=v.empty();// this function is used to check wheter the vector is empty or not it return 1 when empty and 0 when not empty
+    cout<< isEmpty;
+    
+    return 0;
+}
